50b781f37a0d12f229a4e0e2cc844f2b131fb496
Василий Петров <saugardas@yandex.ru>
2016-04-20 22:27:06 +0300

    9th

---
+++ 9/9.rb
@@ -0,0 +1,12 @@
+# Существует только 1 тройка Пифагора, для которой a+b+c=1000. Найти a*b*c
+catch :done do
+  (1..499).each do |a|
+    ((500-a)..(999-a)).each do |b|
+      c = 1000-a-b
+        p a*b*c and throw(:done) if (a + b + c) == 1000 && a*a+b*b == c*c
+    end
+  end
+end
+
+
+
