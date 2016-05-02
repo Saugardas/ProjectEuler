86ce5ac0fd26335ae1d4e201690dd2b6c46a4177
Василий Петров <saugardas@yandex.ru>
2016-05-02 13:54:47 +0300

    37th

---
+++ 037/37.rb
@@ -0,0 +1,16 @@
+# Найти сумму 11 простых чисел, что при отсекании цифр и слева и справа остаются простыми
+
+require 'prime'
+
+primes = []
+Prime.each do |prime|
+  next if prime < 10
+  digits = prime.to_s.chars
+  next if (digits & %w(0 4 6 8)).any? # если останутся при обрезании 5, 9 или чётные - значит не простое
+  left_truncate = Array.new(digits.size){|i| digits[i..-1].join.to_i}
+  right_truncate = Array.new(digits.size){|i| digits[0..i].join.to_i}
+  primes << prime if left_truncate.all?(&:prime?) && right_truncate.all?(&:prime?)
+  break if primes.size == 11
+end
+
+p primes.reduce :+ # 748317
