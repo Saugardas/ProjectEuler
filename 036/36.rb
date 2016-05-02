ce8de19fd5a4d2e9c50f7034a0f1663ccf878283
Василий Петров <saugardas@yandex.ru>
2016-05-02 12:04:26 +0300

    36th

---
+++ 036/36.rb
@@ -0,0 +1,3 @@
+# Найти сумму чисел, что в 10-м и 2-м предтавлении - палиндромы (585 = 1001001001) - 872187
+
+p 1.step(999_999, 2).inject{|sum, num| (num.to_s == num.to_s.reverse && num.to_s(2) == num.to_s(2).reverse) ? sum + num : sum}
