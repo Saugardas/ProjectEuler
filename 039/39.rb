6f97b97d1a82ef41916bb7944bc05e44bb243ab4
Василий Петров <saugardas@yandex.ru>
2016-05-03 14:45:35 +0300

    39th
    fix

---
+++ 039/39.rb
@@ -0,0 +1,20 @@
+# Некоторый числа - есть сумма пифагоровых троек
+# Найти число, <= 1000, имеющее наибольшое число троек
+
+MAX = 1_000
+
+def get_pythagorean_triples  m, n
+  c = m*m + n*n
+  Array.new(MAX/c){|i| [(i+1)*2*m*n, (i+1)*(m*m - n*n), (i+1)*c].sort}
+end
+
+max_c = Math.sqrt(MAX).floor  # ~ максимальная гипотенуза
+arr = []
+
+1.upto(max_c - 1) do |i|
+  (i + 1).upto(max_c) do |y|
+    arr += get_pythagorean_triples(y, i)
+  end
+end
+
+p arr.uniq.map{|el| el.reduce(:+)}.select{|el| el <= MAX}.each_with_object(Hash.new(0)){|el, h| h[el] += 1}.sort_by(&:last).last
