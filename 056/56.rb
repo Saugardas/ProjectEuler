71dcb22c84cff24f74b083e42243d7dbefba5af2
Василий Петров <saugardas@yandex.ru>
2016-05-14 10:42:00 +0300

    56
    f

---
+++ 056/56.rb
@@ -0,0 +1,10 @@
+# найти среди чисел a^b (a, b < 100) число с наибольшей суммой цифр (972)
+arr = []
+2.upto(99) do |a|
+  2.upto(99) do |b|
+    arr << (a**b).to_s.chars.map(&:to_i).reduce(:+)
+  end
+end
+p arr.max
+
+p (2..99).flat_map{|a| (2..99).map{|b| (a**b).to_s.chars.map(&:to_i).reduce(:+) } }.max
