5db1fc470fb97df4953d45f2a76bc9d8f942f35b
Василий Петров <saugardas@yandex.ru>
2016-05-25 22:57:36 +0300

    92

---
+++ 092/92.rb
@@ -0,0 +1,21 @@
+# Количество чисел менее 10млн, цикл суммы квадратов цифр которых сходится к 89(8581146)
+
+def sum_of_squares num
+  num.to_s.chars.map{|i| i.to_i**2}.reduce(:+)
+end
+
+@numbers = {89 => true, 1 => false} # без хеша 2мин, с ни - 57 сек
+
+def correct_number? number
+  num = number
+  loop do
+    num = sum_of_squares(num)
+    if @numbers.has_key?(num)
+      @numbers[number] = @numbers[num]
+      return @numbers[num]
+    end
+  end
+end
+
+p (1..9_999_999).count{|num| correct_number?(num)}
+
