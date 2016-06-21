7ad5a13325951e25a3b559a6a10efb0ffea8b0a3
Василий Петров <saugardas@yandex.ru>
2016-06-21 22:04:15 +0300

    80

---
+++ solutions/080/80.rb
@@ -0,0 +1,53 @@
+# Для первых ста натуральных чисел, корень которых иррационален, найти сумму первых 100 цифр в десятичном представлении и сложить эти сто сумм
+# (40886)
+
+require_relative '../../extension'
+require 'bigdecimal'
+
+# используя BigDecimal =================================================================================================
+p (2..99).reject(&:square?).map { |num| BigDecimal(num).sqrt(100).to_s[2..101].chars.map(&:to_i).reduce(:+) }.reduce(:+)
+
+# используя разложение в цепную дробь ==================================================================================
+
+def get_extended_period(cont)
+  (cont * (200 / cont.size))[0..200]
+end
+
+# возвращаем приближение из числа в виде цепной дроби
+# int_part - целая часть, cont_part - период цепной дроби
+def from_continued(int_part, cont_part)
+  common_fract = 0 # дробная часть
+  # если период дроби чётный - берём для приблежения его, кроме последнего числа
+  if cont_part.size.even?
+    cont_part = cont_part[0..-2]
+    # если период нечётный - то двойной период без последнего числа
+  else
+    cont_part = (cont_part*2)[0..-2]
+  end
+  cont_part.reverse_each do |fr|
+    common_fract = Rational(1.0, (fr + common_fract))
+  end
+  common_fract + int_part
+end
+
+# limit символов после запятой
+def find_digits(numer, denom, limit)
+  digs = []
+  limit.times do
+    res, numer = (numer * 10).divmod(denom)
+    digs << res
+  end
+  digs
+end
+
+# находим сумму первых ста цифр числа
+def get_sum_of_digits(number)
+  # находим цепную дробь
+  big_fract = number.continued_fraction
+  # преобразуем её в большую дробь
+  num = from_continued(0, get_extended_period(big_fract.last))
+  # одно число до запятой и 99 после
+  big_fract.first + find_digits(num.numerator, num.denominator, 99).reduce(:+)
+end
+
+p (2..99).reject(&:square?).map { |num| get_sum_of_digits(num) }.reduce(:+)
