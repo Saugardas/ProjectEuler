0f9dbcf9175028a52f7db2ab43a8f7608f3cedf2
Василий Петров <saugardas@yandex.ru>
2016-12-06 22:00:24 +0300

    76 counting summations

---
+++ solutions/076/76.rb
@@ -0,0 +1,34 @@
+# 190_569_291
+
+# центрированные пятиугольные числа
+def generalized_pentagonal(max)
+  n = 1
+  loop do
+    result = (3 * n**2 - n)/2
+    break if result > max
+    yield result, n
+    result = (3 * n**2 + n)/2
+    break if result > max
+    yield result, n
+    n += 1
+  end
+end
+
+# чередуем каждые 2 элемента
+def sign(number)
+  number.odd? ? :+ : :-
+end
+
+# p(n) = p(n - 1) + p(n - 2) - p(n - 5) - p(n - 7) ...
+@part = { 0 => 1, 1 => 1 }
+def partition(number)
+  return @part[number] if @part.key?(number)
+  result = 0
+  generalized_pentagonal(number) do |pentag, index|
+    result = result.send(sign(index), partition(number - pentag))
+  end
+  @part[number] = result # записываем в хеш, чтобы не высчитывать повторно
+  result
+end
+
+p partition(100) - 1
