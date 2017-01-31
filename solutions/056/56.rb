d82d2d423b651eb2cf2720a1e4dbe40da92c4113
Василий Петров <saugardas@yandex.ru>
2017-01-31 23:16:13 +0300

    56: Use product instead of two cycles

--- solutions/056/56.rb
+++ solutions/056/56.rb
@@ -1,10 +1,4 @@
 # найти среди чисел a^b (a, b < 100) число с наибольшей суммой цифр (972)
-arr = []
-2.upto(99) do |a|
-  2.upto(99) do |b|
-    arr << (a**b).digits.sum
-  end
-end
-p arr.max
+p [*2..99].product([*2..99]).map { |a, b| (a**b).digits.sum }.max
 
 p (2..99).flat_map { |a| (2..99).map { |b| (a**b).digits.sum } }.max
