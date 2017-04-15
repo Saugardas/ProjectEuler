1fd146f1e6b010943f3b45f2a12d0c92e867622a
Василий Петров <saugardas@yandex.ru>
2017-04-15 10:45:29 +0300

    95: Finding divisors sums using sieve

--- solutions/095/95.rb
+++ solutions/095/95.rb
@@ -1,22 +1,21 @@
 # 14316
-require_relative '../../lib/number_theory'
+MAX = 1_000_000
 
-def divisors_sum(number)
-  NumberTheory.divisors_list(number).sum
+def get_divisors_sum(number)
+  array = Array.new(number + 1, 0)
+  1.upto(number / 2) { |i| (2 * i).step(number, i) { |j| array[j] += i } }
+  array
 end
 
-@not_cycled = {}
+@array = get_divisors_sum(MAX)
 def find_chain_size(number)
   list = [number]
   loop do
-    next_number = divisors_sum(list.last)
+    next_number = @array[list.last]
     return list.size if next_number == number
-    if @not_cycled[next_number] || next_number > 1_000_000 || list.include?(next_number)
-      @not_cycled[number] = true
-      return 0
-    end
+    return 0 if next_number > 1_000_000 || list.include?(next_number)
     list << next_number
   end
 end
 
-p 1.upto(1_000_000).map { |i| [i, find_chain_size(i)] }.max_by(&:last)
+p [*1..MAX].map { |i| [i, find_chain_size(i)] }.max_by(&:last)
