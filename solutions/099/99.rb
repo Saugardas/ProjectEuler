7d001b340a900711e07690db31b47df4636837d5
Василий Петров <saugardas@yandex.ru>
2017-05-03 21:49:27 +0300

    99: add offset

--- solutions/099/99.rb
+++ solutions/099/99.rb
@@ -1,4 +1,4 @@
 # 709
 
 logarithms = File.readlines('PE_99').map { |el| el.scan(/\d+/).reduce { |base, exp| exp.to_i * Math.log(base.to_i) } }
-p logarithms.each_with_index.max
+p logarithms.each.with_index(1).max
